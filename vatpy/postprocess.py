616dde0ef5f1318fb20e30f1c4cf0e62940ef165
Jonathan Petersson <astrojpetersson@gmail.com>
2025-09-05 15:23:33 +0200

    update to post-processing script

--- vatpy/postprocess.py
+++ vatpy/postprocess.py
@@ -31,10 +31,6 @@ def copy_group(source_group, dest_group):
             copy_group(source_group[key], new_group)
 
         elif isinstance(source_group[key], h5py.Dataset):
-            # Copy attributes to the group:
-            for attr_name, attr_value in source_group[key].attrs.items():
-                dest_group[key].attrs[attr_name] = attr_value
-
             # Copy dataset to the group:
             dest_group.create_dataset(
                 key,
@@ -43,6 +39,10 @@ def copy_group(source_group, dest_group):
                 compression=source_group[key].compression,
                 chunks=source_group[key].chunks
             )
+
+            # Copy attributes to the group:
+            for attr_name, attr_value in source_group[key].attrs.items():
+                dest_group[key].attrs[attr_name] = attr_value
 
     return None
 
@@ -59,7 +59,10 @@ def copy_hdf5_file(source_file, destination_file):
             # Recursively copy all groups and datasets:
             copy_group(src, dest)
 
-    return None
+            # Time of snapshot:
+            snap_time = src['Header'].attrs['Time']
+
+    return snap_time
 
 
 def do_sweep_post_process(source_dir, snap_range):
@@ -70,42 +73,44 @@ def do_sweep_post_process(source_dir, snap_range):
     '''
     print('\nWelcome to this Arepo Sweep post-processing script ' +
           '(built using Vatpy)')
-    print(f'INFORMATION: Directory of snapshots to post-process: {source_dir}')
-    print('INFORMATION: Range of snapshots to post-process: '
+    print(f'  * Directory of snapshots to post-process: {source_dir}')
+    print('  * Range of snapshots to post-process: '
           f'({snap_range[0]}, {snap_range[1]})')
+    print('\nStarting the post-processing routine...\n')
 
     # Get current working directory:
     cwd = os.getcwd()
 
     # Check if neccessary files are present:
-    print('INITIALISING: Checking directory for requirred files needed to ' +
-          'post-process Arepo snapshots')
-    if not os.path.isfile(f'{cwd}/Arepo'):
-        return 1
-    if os.path.isfile(f'{cwd}/job.sh'):
-        return 1
-    if os.path.isfile(f'{cwd}/param.txt'):
-        return 1
-    if os.path.isfile(f'{cwd}/TreeCol_lookup.dat'):
-        return 1
+    print('INIT: Checking directory for requirred files needed to ' +
+          'post-process snapshots')
+    terminate = '(terminating this script)\n\nTerminating...\n\n'
+    #if not os.path.isfile(f'{cwd}/Arepo'):
+    #    return print(f'ERROR: Missing Arepo file {terminate}')
+    if not os.path.isfile('job.sh'):
+        return print(f'ERROR: Missing job.sh file {terminate}')
+    if not os.path.isfile('param.txt'):
+        return print(f'ERROR: Missing param.txt file {terminate}')
+    if not os.path.isfile('TreeCol_lookup.dat'):
+        return print(f'ERROR: Missing TreeCol_lookup.dat file {terminate}')
 
     # Check if post-process directory and snapshots already exist(s):
-    print('INITIALISING: Checking if post-processed snapshots already exist')
-    snap_start = snap_range[0]
-    snap_end = snap_range[1]
-    if os.path.isdir(f'{cwd}/POSTPROCESSEDSNAPHOTS'):
+    print('INIT: Checking if post-processed snapshots already exist in ' +
+          'POSTPROCESSEDSNAPHOTS')
+    snap_start = int(snap_range[0])
+    snap_end = int(snap_range[1])
+    if os.path.isdir('POSTPROCESSEDSNAPHOTS'):
         snap_str = '000'[:3-len(str(snap_start))] + str(snap_start)
-        while os.path.isfile(f'{cwd}/POSTPROCESSEDSNAPHOTS/snap_{snap_str}'):
+        while os.path.isfile(f'POSTPROCESSEDSNAPHOTS/snap_{snap_str}.hdf5'):
             snap_start += 1
+            if snap_start == snap_end:
+                print('ENDING: All snapshots are already post-processed!\n')
+                return 0
             snap_str = '000'[:3-len(str(snap_start))] + str(snap_start)
-        print(f'INITIALISING: Found {snap_start - snap_range[0]} already ' +
+        print(f'INIT: Found {snap_start - int(snap_range[0])} already ' +
               'post-processed snapshots')
     else:
-        os.makedir(f'{cwd}/POSTPROCESSEDSNAPHOTS')
-
-    if snap_start == snap_end:
-        print('  ENDING: All snapshots are already post-processed!')
-        return 0
+        os.system('mkdir POSTPROCESSEDSNAPHOTS')
 
     # Find user name:
     username = getpass.getuser()
@@ -113,33 +118,81 @@ def do_sweep_post_process(source_dir, snap_range):
     # Find job name:
     with open(cwd + '/job.sh', 'r') as file:
         for line in file:
-            jobmatch = re.search(r'--job-name (\S+)', line)
-            if jobmatch:
-                jobname = jobmatch.group(1)
+            match_job = re.search(r'--job-name (\S+)', line)
+            match_sweep = re.search(r'NUM_SWEEP_ITERATIONS=(\S+)', line)
+            match_snap = re.search(r'NUM_ITERATIONS_PER_SNAPSHOT=(\S+)', line)
+            if match_job:
+                jobname = match_job.group(1)
+            if match_sweep:
+                num_sweep_iterations = int(match_sweep.group(1))
+            if match_snap:
+                num_iterations_per_snapshot = int(match_snap.group(1))
 
-    print('INITIALISING: About to post-process snapshots for user={username}' +
-          f'using job-name={jobname}')
+    print('INIT: Information to track when submitting jobs to SLURM:' +
+          f'\n  USER: {username}\n  JOB-NAME: {jobname}')
 
     # Loop over remaining snapshots at source to post-process:
-    for s in range(snap_start, snap_end):
+    for s in range(snap_start, snap_end+1):
         # Copy the source snapshot to the current working directory:
-        source_file = (source_dir + '/snap' + '000'[:3-len(str(snap_start))] +
-                       str(snap_start) + '.hdf5')
+        source_file = (source_dir + '/snap_' + '000'[:3-len(str(s))] +
+                       str(s) + '.hdf5')
         destination_file = cwd + '/postprocess.hdf5'
-        copy_hdf5_file(source_file=source_file,
-                       destination_file=destination_file)
+        print(f'PREP: Copying data from snapshot {s} to postprocess.hdf5')
+        snap_time = copy_hdf5_file(source_file=source_file,
+                                   destination_file=destination_file)
+
+        # Read the default parameter file:
+        with open('param.txt', 'r') as param:
+            param_lines = param.readlines()
+
+        for line in param_lines:
+            if line.strip().startswith('MaxSizeTimestep'):
+                delta_time = float(line.split()[-1])
+
+        # Copy all parameters to a new parameter file,
+        # but change TimeBegin and TimeMax:
+        print('PREP: Creating a temporary parameter file')
+        with open('param_postprocess.txt', 'w') as param_postprocess:
+            for line in param_lines:
+                if line.strip().startswith('TimeBegin'):
+                    param_postprocess.write(f'TimeBegin    {snap_time}\n')
+                elif line.strip().startswith('TimeMax'):
+                    new_time_max = snap_time + delta_time
+                    param_postprocess.write(f'TimeMax    {new_time_max}\n')
+                elif line.strip().startswith('MaxSizeTimestep'):
+                    new_timestep_max = delta_time / num_sweep_iterations
+                    param_postprocess.write(
+                        f'MaxSizeTimestep    {new_timestep_max}\n')
+                else:
+                    param_postprocess.write(line)
 
         # Launch job:
-        print('LAUNCH: submitting post-processing job for snapshot {s}')
+        print(f'LAUNCH: About to post-process snapshot {s}')
         os.system('sbatch -Q job.sh')
 
-        # 
+        # Automatically check if the job is still running or not:
         time_start = time.time()
         while os.popen('squeue -u ' + username + ' -h -n ' + jobname +
-                       '-o "%.8j"').read():
-            time.sleep(2)
-            print(f'Elapsed time: {int(time.time() - time_start)} s    ',
+                       ' -o "%.8j"').read():
+            time.sleep(1)
+            print(f'  ELAPSED TIME: {int(time.time() - time_start)} s       ',
                   end='\r')
+
+        time_end = time.time()
+        duration = int(time_end - time_start)
+        print(f'COMPLETED: Snapshot {s} post-processed succesfully ' +
+              f'(took {duration} s)       ')
+
+        # Move the final output to another directory:
+        print('OUTPUT: Moving the final output to POSTPROCESSEDSNAPHOTS')
+        final = num_sweep_iterations // num_iterations_per_snapshot
+        f_str = '000'[:3-len(str(final))] + str(final)
+        s_str = '000'[:3-len(str(s))] + str(s)
+        os.system(f'mv OUTPUT/snap_{f_str}.hdf5 POSTPROCESSEDSNAPHOTS/' +
+                  f'snap_{s_str}.hdf5')
+        print(f'DONE: Post-processing of snapshot {s} completed')
+
+    print('ENDING: all snaphots post-processed!\n')
 
     return 0
 
