dc5bae82e1aad7022fa045522f4f673be380cfdd
Jonathan Petersson <astrojpetersson@gmail.com>
2026-05-06 21:38:02 +0200

    update to get star formation data routine

--- vatpy/get_data/get_star_formation_data.py
+++ vatpy/get_data/get_star_formation_data.py
@@ -20,10 +20,12 @@ def get_star_formation_data(output_dir, n, N, dt, H=None, rbins=None):
     birthtime_list = []
     birthmass_list = []
     totalgasmass_list = []
+    totalstellarmass_list = []
     if rbins is not None:
         [birthtime_list.append([]) for i in rbins]
         [birthmass_list.append([]) for i in rbins]
         [totalgasmass_list.append([]) for i in rbins]
+        [totalstellarmass_list.append([]) for i in rbins]
     halfmassradius_list = []
 
     counter = 0
@@ -42,32 +44,39 @@ def get_star_formation_data(output_dir, n, N, dt, H=None, rbins=None):
             # counter = time
             data['Time0'] = time
 
-        # Half-mass radius:
+        # Stellar component:
+        N2 = np.sum(h['Header'].attrs['NumPart_Total'][2])
+        N3 = np.sum(h['Header'].attrs['NumPart_Total'][3])
+        N4 = np.sum(h['Header'].attrs['NumPart_Total'][4])
+        pos_stellar = np.zeros((int(N2 + N3 + N4), 3))
+        mass_stellar = np.zeros(int(N2 + N3 + N4))
+
+        if 'PartType2' in h.keys():
+            mass_stellar[:N2] = (h['PartType2']['Masses'] * iu['umass'] /
+                                 const['Msol'])
+            pos_stellar[:N2] = (h['PartType2']['Coordinates'] * iu['ulength'] /
+                                const['kpc'])
+
+        if 'PartType3' in h.keys():
+            mass_stellar[N2:N2+N3] = (h['PartType3']['Masses'] * iu['umass'] /
+                                      const['Msol'])
+            pos_stellar[N2:N2+N3] = (h['PartType3']['Coordinates'] *
+                                     iu['ulength'] / const['kpc'])
+
+        if 'PartType4' in h.keys():
+            mass_stellar[N2+N3:] = (h['PartType4']['Masses'] * iu['umass'] /
+                                    const['Msol'])
+            pos_stellar[N2+N3:] = (h['PartType4']['Coordinates'] *
+                                   iu['ulength'] / const['kpc'])
+
+        # Stellar half-mass radius:
         if H is not None:
-            N2 = np.sum(h['Header'].attrs['NumPart_Total'][2])
-            N4 = np.sum(h['Header'].attrs['NumPart_Total'][4])
-            totalstellarmass = np.zeros(int(N2 + N4))
-            totalstellarpos = np.zeros((int(N2 + N4), 3))
-
-            if 'PartType2' in h.keys():
-                totalstellarmass[:N2] = (h['PartType2']['Masses'] * iu['umass']
-                                         / const['Msol'])
-                totalstellarpos[:N2] = (h['PartType2']['Coordinates'] *
-                                        iu['ulength'] / const['kpc'])
-
-            if 'PartType4' in h.keys():
-                totalstellarmass[N2:] = (h['PartType4']['Masses'] * iu['umass']
-                                         / const['Msol'])
-                totalstellarpos[N2:] = (h['PartType4']['Coordinates'] *
-                                        iu['ulength'] / const['kpc'])
-
-            # Half-mass radius calculation:
-            if (N2 + N4) > 0:
-                totalstellarradius = np.linalg.norm(totalstellarpos -
-                                                    boxsize/2, axis=1)
-                tsm_sorted = totalstellarmass[np.argsort(totalstellarradius)]
+            if (N2 + N3 + N4) > 0:
+                radius_stellar = np.linalg.norm(pos_stellar - boxsize / 2,
+                                                axis=1)
+                tsm_sorted = mass_stellar[np.argsort(radius_stellar)]
                 csm = np.cumsum(tsm_sorted)
-                tsr_sorted = totalstellarradius[np.argsort(totalstellarradius)]
+                tsr_sorted = radius_stellar[np.argsort(radius_stellar)]
                 halfmassindex = np.argmin(np.abs(csm - np.sum(tsm_sorted) / 2))
                 halfmassradius = tsr_sorted[halfmassindex]
             else:
@@ -83,23 +92,45 @@ def get_star_formation_data(output_dir, n, N, dt, H=None, rbins=None):
         # Within radial bins:
         if rbins is not None:
             for j in range(0, len(rbins)):
-                mask_radius = (np.linalg.norm(pos_gas - boxsize/2, axis=1) <
-                               rbins[j])
-                if len(mass_gas[mask_radius] > 0):
-                    totalgasmass = np.sum(mass_gas[mask_radius])
+                mask_radius_gas = (np.linalg.norm(pos_gas - boxsize/2, axis=1)
+                                   < rbins[j])
+                mask_radius_stellar = (np.linalg.norm(pos_stellar - boxsize/2,
+                                                      axis=1) < rbins[j])
+
+                if len(mass_gas[mask_radius_gas] > 0):
+                    totalgasmass = np.sum(mass_gas[mask_radius_gas])
                 else:
                     totalgasmass = 0
+
+                if len(mass_stellar[mask_radius_stellar] > 0):
+                    totalstellarmass = np.sum(
+                        mass_stellar[mask_radius_stellar])
+                else:
+                    totalstellarmass = 0
+
                 totalgasmass_list[j].append(totalgasmass)
+                totalstellarmass_list[j].append(totalstellarmass)
 
         # Within H times the half mass radius:
         if H is not None:
-            mask_radius = (np.linalg.norm(pos_gas - boxsize/2, axis=1) <
-                           (H * halfmassradius))
-            if len(mass_gas[mask_radius] > 0):
-                totalgasmass = np.sum(mass_gas[mask_radius])
+            mask_radius_gas = (np.linalg.norm(pos_gas - boxsize/2, axis=1)
+                               < (H * halfmassradius))
+            mask_radius_stellar = (np.linalg.norm(pos_stellar - boxsize/2,
+                                                  axis=1)
+                                   < (H * halfmassradius))
+
+            if len(mass_gas[mask_radius_gas] > 0):
+                totalgasmass = np.sum(mass_gas[mask_radius_gas])
             else:
                 totalgasmass = 0
+
+            if len(mass_stellar[mask_radius_stellar] > 0):
+                totalstellarmass = np.sum(mass_stellar[mask_radius_stellar])
+            else:
+                totalstellarmass = 0
+
             totalgasmass_list.append(totalgasmass)
+            totalstellarmass_list.append(totalstellarmass)
 
         # Star formation:
         if 'PartType4' in h.keys():
@@ -164,22 +195,22 @@ def get_star_formation_data(output_dir, n, N, dt, H=None, rbins=None):
     # sSFR calculation:
     if rbins is not None:
         sSFR_list = []
-        totalgasmass_mean_list = []
+        totalstellarmass_mean_list = []
         for i in range(0, len(rbins)):
-            totalgasmass_mean_list.append(
+            totalstellarmass_mean_list.append(
                 np.histogram(time_list, bins=bins_time,
-                             weights=totalgasmass_list[i])[0]
+                             weights=totalstellarmass_list[i])[0]
                 / np.histogram(time_list, bins=bins_time)[0])
-            sSFR_list.append(SFR_list[i] / totalgasmass_mean_list[i])
+            sSFR_list.append(SFR_list[i] / totalstellarmass_mean_list[i])
         data['sSFR'] = sSFR_list
-        data['GasMass'] = totalgasmass_mean_list
+        data['StellarMass'] = totalstellarmass_mean_list
     else:
-        totalgasmass_mean = (np.histogram(time_list, bins=bins_time,
-                                          weights=totalgasmass_list)[0] /
-                             np.histogram(time_list, bins=bins_time)[0])
-        sSFR = SFR / totalgasmass_mean
+        totalstellarmass_mean = (np.histogram(time_list, bins=bins_time,
+                                              weights=totalstellarmass_list)[0]
+                                 / np.histogram(time_list, bins=bins_time)[0])
+        sSFR = SFR / totalstellarmass_mean
         data['sSFR'] = sSFR
-        data['GasMass'] = totalgasmass_mean
+        data['StellarMass'] = totalstellarmass_mean
 
     if H is not None:
         halfmassradius_mean = (np.histogram(time_list, bins=bins_time,
